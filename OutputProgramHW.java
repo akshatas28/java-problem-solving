3335591e3d561c15295e48905aa07277a7d1c281
Pradeep Saptasagar <pradeep.saptasagar22@pccoepune.org>
2026-03-25 11:21:57 +0530

    Lecture 2 part 1

---
+++ OutputProgramHW.java
@@ -0,0 +1,14 @@
+public class OutputProgramHW {
+    public static void main(String args[]) {
+        // output program
+        System.out.print("Hello World");
+        System.out.print("Hello World"); // if we use print: we get output: Hello WorldHello World
+        System.out.println("Hello World");
+        System.out.print("Hello World"); // if we use println: we get output: Hello World (and then next in next line)
+                                         // Hello World
+        System.out.print("Hello World\n");
+        System.out.print("Hello World"); // if we use \n also: we get output: Hello World (and then next in next line)
+                                         // Hello World
+        System.out.print("Hello World\n Lecture 2"); // post /n everything will print along with spaces
+    }
+}
