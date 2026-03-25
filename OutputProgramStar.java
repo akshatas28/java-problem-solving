39cee514227aadf6d69a7c699117f0bc70c5f59c
Pradeep Saptasagar <pradeep.saptasagar22@pccoepune.org>
2026-03-25 11:43:02 +0530

    Lecture 2 part 2

---
+++ OutputProgramStar.java
@@ -0,0 +1,10 @@
+public class OutputProgramStar {
+    public static void main(String args[]) {
+        // output program
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *"); // javac OutputProgramStar.java --> java OutputProgramStar
+        System.out.println("*\n**");
+    }
+}
