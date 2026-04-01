611950cd8b18f92ec4504c00929c7023d6197cb3
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-01 18:48:56 +0530

    Lecture 2 part 3

--- Lecture_2/Sum.java
+++ Lecture_2/Sum.java
@@ -1,4 +1,5 @@
 public class Sum {
+    // code for understanding mathematical fundamentals
     public static void main(String[] args) {
         int a = 10; // variable declarations
         int b = 25;
@@ -6,12 +7,14 @@ public class Sum {
         System.out.println(sum); // output = 35
         int mul = a * b; // multiplication operation
         int diff = a - b; // difference operation
+        float mod = a % b;
         System.out.println(mul); // 250
         System.out.println(diff); // -15
         System.out.println(((a * b) / (a - b))); // -16
         System.out.println(((a * b) / (b - a))); // 16
         float c = ((a * b) / (b - a));
         System.out.println(c); // 16.0
+        System.out.println(mod); // 10.0
         System.out.println("sum class over");
     }
 }
