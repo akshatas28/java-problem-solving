a27c03f290fae4c25d85ec90d456991a90100ea9
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-01 20:18:09 +0530

    Lecture 3 part 6

---
+++ Lecture_3/Calculator.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+//code for checking switch case condition
+//to create a calculator for add, sub, mul, div, mod using 2 numbers
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		// a and b are integers
+		System.out.println("Enter opea" +
+				"ration: add, sub, mul, div, mod");
+		String operation = sc.next();
+		switch (operation) {
+			case "add": {
+				int result = a + b;
+				System.out.println(result);
+				break;
+			}
+			case "sub": {
+				int result = a - b;
+				System.out.println(result);
+				break;
+			}
+			case "mul": {
+				int result = a * b;
+				System.out.println(result);
+				break;
+			}
+			case "div": {
+				int result = a / b;
+				System.out.println(result);
+				break;
+			}
+			case "mod": {
+				int result = a % b;
+				System.out.println(result);
+				break;
+			}
+			default: {
+				System.out.println("NA");
+			}
+		}
+	}
+}
