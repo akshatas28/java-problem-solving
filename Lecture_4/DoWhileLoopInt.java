c0462ba14a4eaeafec9c6af49d19a7b246957bb2
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-02 13:49:59 +0530

    Lecture 4 part 5

---
+++ Lecture_4/DoWhileLoopInt.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class DoWhileLoopInt {
+	public static void main(String[] args) {
+		// code for do while loop
+		// print 0-10 numbers list
+		// initialize i
+		int i = 0;
+		// initialize do loop
+		do {
+			System.out.println(i); // prints one after another else print(i+" ") for ome after another in same line
+			// increment i
+			i++;
+		} while (i < 11); // add while condition at end with a terminator that is semi colon mark
+	}
+}
