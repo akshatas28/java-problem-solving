60468fd4b60a44c11fd626b1ba94be19d2e5d1ad
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-02 13:43:59 +0530

    Lecture 4 part 2

---
+++ Lecture_4/ForLoopInt.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ForLoopInt {
+	public static void main(String[] args) {
+		// learning for loop
+		// print numbers 0 to 10 using for loop
+		// i++ => i= i+1
+		for (int i = 0; i < 11; i++) {
+			// directly print i
+			System.out.println(i);
+		}
+	}
+}
