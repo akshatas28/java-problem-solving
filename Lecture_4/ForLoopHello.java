cf6654ba1d5eab84e338b36d4f9fc2237d014856
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-02 13:40:39 +0530

    Lecture 4 part 1

---
+++ Lecture_4/ForLoopHello.java
@@ -0,0 +1,9 @@
+public class ForLoopHello {
+	public static void main(String[] args) {
+		// learning for loop
+		// print hello 3 times using for loop
+		for (int i = 0; i < 3; i++) {
+			System.out.println("hello");
+		}
+	}
+}
