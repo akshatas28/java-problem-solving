738c7574c006f038cee01276631ebed9993a5565
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-03 21:21:48 +0530

    Lecture 4 part 15

---
+++ Lecture_4/WhileEvenNumbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+//print all even numbers till n
+
+public class WhileEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n until which even numbers need to be printed");
+        int n = sc.nextInt();
+        int i = 0;
+        while (i <= n) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                i = i + 2;
+            } else {
+                break;
+            }
+        }
+    }
+}
