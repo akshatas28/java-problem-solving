80bdeded173f42e58b6c16f76abc447eac42a916
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-07 23:55:06 +0530

    Lecture 5 part 13

---
+++ Lecture_5/NumPyramid.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+//print inverted half pyramid rotated 180
+public class NumPyramid {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= (n); i++) {
+			// inner for loop
+			for (int j = 1; j <= n + i; j++) {
+				if (j <= n - i || j >= n + i) {
+					System.out.print(" ");
+				} else if ((i + j) % 2 == 0) {
+					System.out.print(i);
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
