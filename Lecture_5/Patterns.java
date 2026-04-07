3b63e5bd5baee654548827fa5c2fb21ba09754b4
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-07 12:28:02 +0530

    Lecture 5 part 1

--- Lecture_5/Patterns.java
+++ Lecture_5/Patterns.java
@@ -7,7 +7,7 @@ public class Patterns {
         for (int i = 1; i <= 4; i++) {
             // here u can declare 2 vars for 4 and 5 or fetch it from user using scanner
             // class
-            // the beliw is inner for loop
+            // the below is inner for loop
             for (int j = 1; j <= 5; j++) {
                 System.out.print("*");
             }
