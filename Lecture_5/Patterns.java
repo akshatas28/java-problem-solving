552bf13c0e70687d586b62225aa8540d77e1e29d
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-07 12:27:23 +0530

    Lecture 5 part 1

--- Lecture_5/Patterns.java
+++ Lecture_5/Patterns.java
@@ -1,3 +1,17 @@
 import java.util.*;
 
-public class Patterns
+//Print solid rectangle
+public class Patterns {
+    public static void main(String[] args) {
+        // the below is outer for loop
+        for (int i = 1; i <= 4; i++) {
+            // here u can declare 2 vars for 4 and 5 or fetch it from user using scanner
+            // class
+            // the beliw is inner for loop
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
