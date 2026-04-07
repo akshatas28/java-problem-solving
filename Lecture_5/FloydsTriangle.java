a327f7e315e709d0081d4a544e4d521188a448c4
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-07 12:47:52 +0530

    Lecture 5 part 9

---
+++ Lecture_5/FloydsTriangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+//print floyds triangle
+public class FloydsTriangle {
+	public static void main(String[] args) {
+		int n = 4;
+		int num = 1;
+		// outer loop
+		for (int i = 1; i <= n; i++) {
+			// inner loop
+			for (int j = 1; j <= i; j++) {
+				System.out.print(num);
+				num++;
+			}
+			System.out.println();
+		}
+	}
+}
