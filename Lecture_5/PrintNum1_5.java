4e87dbe36e8cf825d0eab6fa0d3972439cf0a9e0
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-07 12:45:55 +0530

    Lecture 5 part 7

--- Lecture_5/PrintNum1_5.java
+++ Lecture_5/PrintNum1_5.java
@@ -3,7 +3,7 @@ import java.util.*;
 //print pattern 1-5
 public class PrintNum1_5 {
 	public static void main(String[] args) {
-		int n = 4;
+		int n = 4; // u can put 5 and print it
 		// outer loop
 		for (int i = 1; i <= n; i++) {
 			// inner loop
