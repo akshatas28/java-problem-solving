11c50a340b24806ad2dda022d3e6cdd80ad95920
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-07 12:45:03 +0530

    Lecture 5 part 7

---
+++ Lecture_5/PrintNum1_5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//print pattern 1-5
+public class PrintNum1_5 {
+	public static void main(String[] args) {
+		int n = 4;
+		// outer loop
+		for (int i = 1; i <= n; i++) {
+			// inner loop
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
