370fbd2dca5f6d26a8d94e313126527e99a24e23
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-08 23:11:54 +0530

    Lecture 6 part 1

--- Lecture_6/PatternsAdvanced.java
+++ Lecture_6/PatternsAdvanced.java
@@ -12,14 +12,18 @@ public class PatternsAdvanced {
             for (int j = 1; j <= space; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= n; j++) {
-                if (i + j >= n + 1) {
-                    System.out.print("*");
-                }
+            // for (int j = 1; j <= n; j++) {
+            // if (i + j >= n + 1) {
+            // System.out.print("*");
+            // }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = n; i >= 1; i--) {
+        for (
+
+                int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
