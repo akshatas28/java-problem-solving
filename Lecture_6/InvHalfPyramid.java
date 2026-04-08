6254cf48ae02a71c9ce02a960fe8951e5afeaea2
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-09 01:22:05 +0530

    Lecture 6 part 11

---
+++ Lecture_6/InvHalfPyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+//print inverted half pyramid
+
+public class InvHalfPyramid {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
