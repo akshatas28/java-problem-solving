be3ab4d6e6d5690a6f786c342b301f73a1b2f08f
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-11 22:18:14 +0530

    Lecture 7 part 9

---
+++ Lecture_7/TableFunc.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TableFunc {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        table(n);
+    }
+
+    public static void table(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n * i);
+        }
+    }
+}
