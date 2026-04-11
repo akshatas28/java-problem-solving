cea26dd2095b766e08fa5bce23abbbf9a75167a5
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-11 21:37:45 +0530

    Lecture 7 part 1

--- Lecture_7/Functions.java
+++ Lecture_7/Functions.java
@@ -1,3 +1,18 @@
 import java.util.*;
 
-public class Functions
+public class Functions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        // call function, function can be written above or below main function
+        printMyName(name);
+        // function block will be returned
+    }
+
+    public static void printMyName(String name) {
+        // whatever output is needed can be specified here
+        System.out.println(name);
+        // return mandatory
+        return;
+    }
+}
