2ff9552c8d46072f7b572a80cddc4ef07dfeb705
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-12 22:31:18 +0530

    Lecture 8 part 7

---
+++ Lecture_8/DisplayFunc.java
@@ -0,0 +1,23 @@
+//7. write program to enter the nums till the user wants and at the end it should display the count of positive, negative and zeros entered
+//8. 2 nums entered by user, x and n: func to find value of one num raised to power of another: x raised to n
+//9. funct that calculates GCD of 2 nums
+//10. print fibonacci series of n terms where n is input by user: 0 1 1 2 3 5..In fibonacci series, a num is sum of previous 2 nums that came before it.
+
+import java.util.*;
+
+public class DisplayFunc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        countDisplay(num);
+    }
+
+    public static void countDisplay(int num) {
+        System.out.println("enter num");
+        int num = sc.nextInt();
+        System.out.println("need to enter more: 1 for yes, 0 for no");
+        int dec = sc.nextInt();
+        if (dec == 0) {
+            
+        }
+    }
+}
