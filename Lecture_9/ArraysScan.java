a935cd3462b9e99f1a3aa7b296c51dab00583301
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-15 00:05:40 +0530

    Lecture 9 part 4

---
+++ Lecture_9/ArraysScan.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+//arrays start by second defn
+public class ArraysScan {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		// int marks[] = new int[3];
+		// marks[0] = 97;
+		// marks[1] = 98;
+		// marks[2] = 99;
+		int marks[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			System.out.println(marks[i]);
+		}
+	}
+}
