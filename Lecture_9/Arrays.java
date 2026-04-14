098b203f82f0c19a2ab714b7e6b31af295506c60
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-15 00:01:11 +0530

    Lecture 9 part 1

---
+++ Lecture_9/Arrays.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+//arrays start
+public class Arrays {
+	public static void main(String[] args) {
+		int[] marks = new int[3];
+		marks[0] = 97;
+		marks[1] = 97;
+		marks[2] = 97;
+		System.out.println(marks[0]);
+	}
+}
