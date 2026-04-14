af221d264306a88151c0a72d0cf22d0f3315430e
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-15 00:04:26 +0530

    Lecture 9 part 3

---
+++ Lecture_9/Arrays2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+//arrays start by second defn
+public class Arrays2 {
+	public static void main(String[] args) {
+		// int marks[] = new int[3];
+		// marks[0] = 97;
+		// marks[1] = 98;
+		// marks[2] = 99;
+		int marks[] = { 97, 98, 99 };
+		for (int i = 0; i < 3; i++) {
+			System.out.println(marks[i]);
+		}
+	}
+}
