35f7dc120ed140470c07ff89498ae3339dcabdb5
Akshata Saptasagar <akshata.saptasagar@gmail.com>
2026-04-27 15:52:26 +0530

    Lecture 12 part 2

---
+++ Lecture_12/StringConcat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+//lecture 12 - strings : concatenation
+public class StringConcat {
+	public static void main(String[] args) {
+		String s1 = "tony";
+		String s2 = "stark";
+		// both s1 and s2 are added and made into one string
+		String s = s1 + s2;
+		System.out.println(s);
+	}
+}
